0df7498581842de31701ab1d9e0bafbde38f03bb
vixuancu <vixuancu2004@gmail.com>
2024-12-11 14:21:06 +0700

    Design Header Responsive

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import "./App.scss";
+import Header from "./components/Header";
+import TableUsers from "./TableUsers";
+import Container from "react-bootstrap/Container";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world React with VIXUANCU</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {/* <Container> */}
+      <Header />
+      <TableUsers />
+      {/* </Container> */}
     </div>
   );
 }
